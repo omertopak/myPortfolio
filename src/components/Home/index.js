75d575c7f7c1ca577f96d32a4a8007604fb026c5
omer <omertopak1@gmail.com>
2024-01-03 17:12:53 +0000

    home page solved

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,8 +5,9 @@ import './index.scss';
 const Home = ( ) => {
     
     return(
+        <>
         <div className="container home-page"> 
-            <div>
+            <div className="text-zone">
                 <h1>Hi<br/> I'm
                 <img src={LogoTitle} alt="developer "/>
                 omertopak
@@ -16,6 +17,7 @@ const Home = ( ) => {
                 <Link to="contact" className='flat-button'>CONTACT ME</Link>
             </div>
         </div>
+        </>
     )
 }
 
