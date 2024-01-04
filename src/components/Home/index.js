1b2fde392c8546175948e45f3c6ca6c66bb38e5d
omer <omertopak1@gmail.com>
2024-01-04 03:56:43 +0000

    about done

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import './index.scss';
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters/index';
 import Logo from './Logo';
+import Loader from 'react-loaders';
 
 const Home = ( ) => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -40,6 +41,7 @@ const Home = ( ) => {
             </div>
             <Logo/>
         </div>
+        <Loader type='pacman'/>
         </>
     )
 }
