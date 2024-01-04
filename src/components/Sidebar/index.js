28db8ee7c7ec5e1064bae93395bc30b504de15c2
omer <omertopak1@gmail.com>
2024-01-04 05:31:45 +0000

    mediaquery sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,15 +3,24 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import {Link, NavLink} from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faHome, faSuitcase, faUser, faBars, faClock, faClose } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-const Sidebar = () => (
+import { useState } from 'react'
+
+
+
+
+const Sidebar = () => {
+    
+    const [showNav, setShowNav] = useState(false)
+    
+    return ( 
     <div className='nav-bar'>
         <Link className='logo' to='/'>
         <img src={LogoS} alt="logo"/>
         <img className="sub-logo" src={LogoSubtitle} alt="logo"/>
         </Link>
-        <nav>
+        <nav className={showNav ? 'mobile-show' : ''}>
             <NavLink exact="true" activeclassname="active" to="/">
                 <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
             </NavLink>
@@ -24,6 +33,12 @@ const Sidebar = () => (
             <NavLink exact="true" activeclassname="active" className="portfolio-link" to="/portfolio">
                 <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
             </NavLink>
+            <FontAwesomeIcon
+            onClick={()=>setShowNav(false)}
+            icon={faClose}
+            color='#ffd700'
+            size='3x'
+            className='close-icon'/>
         </nav>
 
         <ul>
@@ -43,7 +58,13 @@ const Sidebar = () => (
                 </a>
             </li> */}
         </ul>
-    </div>
-)
+        <FontAwesomeIcon
+        onClick={()=>setShowNav(true)}
+        icon={faBars}
+        color="#ffd700"
+        size="3x"
+        className='hamburger-icon'/>
+    </div>)
+}
 
 export default Sidebar
