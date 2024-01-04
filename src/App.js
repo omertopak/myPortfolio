846814fdfe23d4900392bf2097a2e8f2ea26d056
omer <omertopak1@gmail.com>
2024-01-04 05:10:07 +0000

    portfolio page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Layout from './components/Layout'
 import Home from './components/Home'
 import './App.scss';
 import About from './components/About';
+import Contact from './components/contact';
+import Portfolio from './components/Portfolio';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Route path="/" element={<Layout/>}>
         <Route index element={<Home/>}/>
         <Route path="about" element={<About/>}/>
+        <Route path="contact" element={<Contact/>}/>
+        <Route path="portfolio" element={<Portfolio/>}/>
       </Route>
     </Routes>
     </>
