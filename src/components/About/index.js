b3a51067d000f22d622419bdc9c3f68d75ff691d
omer <omertopak1@gmail.com>
2024-01-05 17:17:00 +0000

    up

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,16 +1,25 @@
 import { useEffect, useState } from 'react'
 import {
   faAngular,
+  faBootstrap,
   faCss3,
+  faDochub,
+  faDocker,
   faGitAlt,
   faHtml5,
   faJsSquare,
+  faNode,
+  faNodeJs,
   faReact,
+  faSass,
 } from '@fortawesome/free-brands-svg-icons'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './index.scss'
+import postman from '../../assets/images/Postman.png'
+import mongo from '../../assets/images/mongo.png'
+import express from '../../assets/images/Expressjs.png'
 
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -48,10 +57,10 @@ const About = () => {
           </p>
         </div>
 
-        <div className="stage-cube-cont">
+        {/* <div className="stage-cube-cont">
           <div className="cubespinner">
             <div className="face1">
-              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+              <FontAwesomeIcon icon={faBootstrap} color="#DD0031" />
             </div>
             <div className="face2">
               <FontAwesomeIcon icon={faHtml5} color="#F06529" />
@@ -64,6 +73,30 @@ const About = () => {
             </div>
             <div className="face5">
               <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faSass} color="#EC4D28" />
+            </div>
+          </div>
+        </div> */}
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faNode} color="#68A063" />
+            </div>
+            <div className="face2">
+              {/* <FontAwesomeIcon icon={faHtml5} color="#F06529" /> */}
+              <img src={postman} width="140px"/>
+            </div>
+            <div className="face3">
+              {/* <FontAwesomeIcon icon={faCss3} color="#28A4D9" /> */}
+              <img src={mongo} width="130px"/>
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faDocker} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+            <img src={express} width="140px"/>
             </div>
             <div className="face6">
               <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
