7c22c17ac8f8608885eb0cce1339d8ab83e306db
omer <omertopak1@gmail.com>
2024-01-06 05:26:59 +0000

    up

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,12 +3,13 @@ import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss";
 // import { getDocs, collection } from 'firebase/firestore';
-// import { db } from '../../firebase';
+import portfolioData from '../../assets/data/data.json';
 
 const Portfolio = () => { 
+    console.log(portfolioData);
     const [letterClass, setLetterClass] = useState('text-animate');
-    // const [portfolio, setPortfolio] = useState([]);
-
+     
+    //animasyon letter
     useEffect(() => {
         const timer = setTimeout(() => {
             setLetterClass('text-animate-hover');
@@ -19,43 +20,33 @@ const Portfolio = () => {
         }
     });
 
-    // useEffect(() => {
-    //     getPortfolio();
-    // }, []);
-
-    // const getPortfolio = async () => {
-    //     const querySnapshot = await getDocs(collection(db, 'portfolio'));
-    //     setPortfolio(querySnapshot.docs.map((doc) => doc.data()));
-    // }
-
-    // const renderPortfolio = (portfolio) => {
-    //     return (
-    //         <div className="images-container">
-    //             {
-    //                 portfolio.map((port, idx) => {
-    //                     return (
-    //                         <div className="image-box" key={idx}>
-    //                             <img 
-    //                             src={port.image}
-    //                             className="portfolio-image"
-    //                             alt="portfolio" />
-    //                             <div className="content">
-    //                                 <p className="title">{port.name}</p>
-    //                                 <h4 className="description">{port.description}</h4>
-    //                                 <button
-    //                                     className="btn"
-    //                                     onClick={() => window.open(port.url)}
-    //                                 >View</button>
-    //                             </div>
-    //                         </div>
-    //                     )
-    //                 })
-    //             }
-        //     </div>
-        // );
-    // }
-
-
+    const renderPortfolio = (portfolio) => {
+        return (
+            <div className="images-container">
+                {
+                    portfolio.map((port, idx) => {
+                        return (
+                            <div className="image-box" key={idx}>
+                                <img 
+                                src={port.cover}
+                                className="portfolio-image"
+                                alt="portfolio" />
+                                <h2>{port.cover}</h2>
+                                {/* <div className="content">
+                                    <p className="title">{port.name}</p>
+                                    <h4 className="description">{port.description}</h4>
+                                    <button
+                                        className="btn"
+                                        onClick={() => window.open(port.url)}
+                                    >View</button>
+                                </div> */}
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        );
+    }
     return (
         <>
             <div className="container portfolio-page">
@@ -66,7 +57,9 @@ const Portfolio = () => {
                         idx={15}
                     />
                 </h1>
-                {/* <div>{renderPortfolio(portfolio)}</div> */}
+                <div>{renderPortfolio(portfolioData.portfolio)}</div>
+                <div className="images-container">
+            </div>
             </div>
             <Loader type="line-scale" />
         </>
